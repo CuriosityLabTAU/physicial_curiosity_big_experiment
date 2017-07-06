55df093932542611838655475d3621baa73dd42f
gorengordon <curiosity@gorengordon.com>
2017-07-06 18:50:05 +0300

    for animation

---
+++ for_aminations.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+from naoqi import ALProxy
+import sys
+import almath
+import json
+import time
+import argparse
+
+port = 9559
+robotIP = '192.168.0.100'
+anim=ALProxy("ALAnimatedSpeech",robotIP,port)
+cong={"bodyLnguageMde":"contextual"}
+
+
+anim.say("Thank you very mauch  for your help! I an Matan and I will be here to help you", cong)
